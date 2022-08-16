8846e58e72f868011040fe7fc8b2bd467c3b300e
Tshenolo Lobelo <63031932+Tshenolo1994@users.noreply.github.com>
2022-08-16 12:45:10 +0200

    about changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,15 +45,16 @@ export default function App() {
         <SwiperSlide >
 
   <Flex flexDirection="column" p={12} >
-  <Text fontSize="6xl"  textAlign="left" width='90%' fontFamily="Altone Trial">An easy and secure digital account. Ideal for you or your company</Text>
+  <Text fontSize="4xl" ml={8}  textAlign="left" width='90%' fontFamily="Altone Trial">An easy and secure digital account. Ideal for you or your company</Text>
     <Button width='25%'>Open account</Button>
   </Flex>
-  <Flex w="60%" justifyContent="space-between">
-    <Box className="el el-2"></Box>
+  <Flex w="60%" justifyContent="space-between" gap={8}>
+    <Box className="el el-2" ></Box>
     <Text
    position="absolute"
    bottom="25%"
    textAlign="left"
+ w="60%"
     >African Bank is a financial solution that works as a digital account, for financial management and insertion, aimed at educational and cultural initiatives, commercial establishments and the promotion of tourist equipment.</Text>
   </Flex>
 
@@ -64,7 +65,7 @@ export default function App() {
    position="absolute"
    bottom="20%"
    textAlign="left"
-   w="35%"
+   w="80%"
    mt={8}
     >African Bank is a financial solution that works as a digital account, for financial management and insertion, aimed at educational and cultural initiatives, commercial establishments and the promotion of tourist equipment.</Text>
                 <Box className="el-3"></Box>
@@ -73,10 +74,10 @@ export default function App() {
             left="10%"
             className="text"
             color="rgba(0,24,69, .3)"
-            fontSize="120px" fontFamily="Altone Trial">INVEST</Text>
+            fontSize="100px" fontFamily="Altone Trial">INVEST</Text>
           
             <Text position="absolute" fontFamily="Altone Trial"
-            right="-35%"  fontSize="120px"  color="rgba(0,24,69, .3)"
+            right="-35%"  fontSize="100px"  color="rgba(0,24,69, .3)"
     className="text"
             >INSURE</Text>
         </SwiperSlide>
@@ -85,10 +86,10 @@ export default function App() {
         
             <UnorderedList listStyleType="none" textAlign="left">
             <ListItem fontSize="4xl" mb="2rem" fontFamily="Altone Trial">Why African Bank?</ListItem>
-  <ListItem fontSize="2xl">Honest guidance that helps you choose what works for you.</ListItem>
-  <ListItem fontSize="2xl">Affordable offers that suit your goals and budget.</ListItem>
-  <ListItem fontSize="2xl">Competitive interest rate based on how you spend.</ListItem>
-  <ListItem fontSize="2xl">No hidden fees, just clear pricing and payment plans.t</ListItem>
+  <ListItem fontSize="1xl">Honest guidance that helps you choose what works for you.</ListItem>
+  <ListItem fontSize="1xl">Affordable offers that suit your goals and budget.</ListItem>
+  <ListItem fontSize="1xl">Competitive interest rate based on how you spend.</ListItem>
+  <ListItem fontSize="1xl">No hidden fees, just clear pricing and payment plans.t</ListItem>
 </UnorderedList>
         
         </SwiperSlide>
